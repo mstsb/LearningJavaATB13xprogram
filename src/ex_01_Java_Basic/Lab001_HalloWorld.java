ab07f3d6cc1460117d89dcab08a70b83f90cbf05
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-14 21:32:41 +0530

    updated

--- src/ex_01_Java_Basic/Lab001_HalloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HalloWorld.java
@@ -4,6 +4,6 @@ import org.omg.Messaging.SyncScopeHelper;
 
 public class Lab001_HalloWorld {
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
     }
 }
