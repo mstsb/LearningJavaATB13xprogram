58ec5388132d79ebe32da8e2d22e0014958e7605
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-17 22:16:03 +0530

    printf

---
+++ src/ex_02_Java_Basic/Lab009_printf.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basic;
+
+public class Lab009_printf {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.printf("The Value is : %d", a);
+        System.out.println("");
+        System.out.printf("%d + %d",a,a);
+    }
+}
