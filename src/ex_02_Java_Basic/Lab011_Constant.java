38d30fd450ee8701ec5b06132fc2ed9a84bec2b6
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-17 22:53:28 +0530

    constant using final keyword

---
+++ src/ex_02_Java_Basic/Lab011_Constant.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basic;
+
+public class Lab011_Constant {
+    public static void main(String[] args) {
+        final int a = 10;
+//        a = 20 This is not allowed bcz it is constant using final keyword
+        System.out.println(a);
+    }
+}
