b64ee2656d82322f68c2b4af340c48da6e79a100
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-17 23:08:00 +0530

    final keyword

---
+++ src/ex_03_Literals/Lab012_final.java
@@ -0,0 +1,9 @@
+package ex_03_Literals;
+
+public class Lab012_final {
+    public static void main(String[] args) {
+        final int a = 10;
+//        a = 20 is not allowed bcz of constant using final keyword
+        System.out.println(a);
+    }
+}
