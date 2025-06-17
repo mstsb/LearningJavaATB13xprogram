3fcd2a49999b8ca7c10df97552ade2fbb33475c1
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-17 20:06:15 +0530

    Variable

---
+++ src/ex_02_Java_Basic/Lab006_Variable.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basic;
+
+public class Lab006_Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 65 + 1;
+        System.out.println("The result is : "+ a);
+    }
+}
