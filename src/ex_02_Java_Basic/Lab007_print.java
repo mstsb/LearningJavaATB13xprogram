67d0e5fa9368efbbfa1818505c251a4c5f9e59ae
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-17 22:05:03 +0530

    print

---
+++ src/ex_02_Java_Basic/Lab007_print.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basic;
+
+public class Lab007_print {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print("Hello");
+
+    }
+}
