1dd6414b6fe4ba941567eb5bf1d852e6fa366e6b
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-17 22:07:24 +0530

    println

---
+++ src/ex_02_Java_Basic/Lab008_println.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basic;
+
+public class Lab008_println {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Hello");
+        System.out.println("Hello");
+    }
+}
