44f5150a4c4720a33b3448537a02e2ba4b72a480
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-18 18:00:56 +0530

    Interviewque

---
+++ src/ex_05_Operators/Lab015_Interviewque.java
@@ -0,0 +1,12 @@
+package ex_05_Operators;
+
+public class Lab015_Interviewque {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "World";
+        int c = 10, d = 10;
+        System.out.println(a + b + c + d);
+        System.out.println(c + d + a + b);
+
+    }
+}
