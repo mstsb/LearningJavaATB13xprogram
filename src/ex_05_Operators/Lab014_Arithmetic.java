571c30ddc65416273557372ee6208ae0e06dfee6
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-18 17:55:48 +0530

    Arithmetic

---
+++ src/ex_05_Operators/Lab014_Arithmetic.java
@@ -0,0 +1,16 @@
+package ex_05_Operators;
+
+public class Lab014_Arithmetic {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 3;
+        float c = 20.0f;
+        System.out.println(a/b);
+        System.out.println(c/b);
+        System.out.println("a+b");
+        System.out.println(a+b);
+        System.out.printf("%d + %d",a,b).println();
+        System.out.printf("%d + %d = %d",a,b, a+b).println();
+        System.out.printf("a+b");
+    }
+}
