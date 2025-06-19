f5a17ab14e331745760182554d62e8f8f049bbc7
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-19 08:47:01 +0530

    Arithmeticope

---
+++ src/exam/Arithmeticope.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class Arithmeticope {
+    public static void main(String[] args) {
+        int a = 10 , b = 5;
+        System.out.println("Addition : " + (a+b));
+        System.out.println("Subtraction : " +(a-b));
+        System.out.println("Multiplication : " + (a*b));
+        System.out.println("Division : " + (a/b));
+        System.out.println("MOdulus : " + (a%b));
+
+//        Addition: 15 Subtraction: 5 Multiplication: 50 Division: 2 Modulus: 0
+    }
+}
