0a28b4474414bb91153f3390a5ea40df2991262f
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-19 08:39:18 +0530

    calculation

---
+++ src/exam/calculation.java
@@ -0,0 +1,15 @@
+package exam;
+
+public class calculation {
+    public static void main(String[] args) {
+        int a = 25;
+        double b = 3.14;
+        boolean c = true;
+        char d = 'A';
+        System.out.println("Integer : " + a);
+        System.out.println("Double : " + b);
+        System.out.println("Boolean : " + c);
+        System.out.println("Character : " + d);
+        //Integer: 25 Double: 3.14 Boolean: true Character: A
+    }
+}
