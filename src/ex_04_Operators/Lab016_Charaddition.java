83932f702a03eded4ef3fb1b39ae5a0f21809846
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-19 22:23:28 +0530

    Charaddition

---
+++ src/ex_04_Operators/Lab016_Charaddition.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Lab016_Charaddition {
+    public static void main(String[] args) {
+        char a = 'A' , b = 'B';
+        System.out.println( a + " " + b);
+//        System.out.println( b );
+    }
+}
