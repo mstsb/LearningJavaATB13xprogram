a43c2327baf76817cde8535a11fb644def894a8c
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-19 22:24:43 +0530

    interviewque

--- src/ex_04_Operators/Lab015_Interviewque.java
+++ src/ex_04_Operators/Lab015_Interviewque.java
@@ -1,4 +1,4 @@
-package ex_05_Operators;
+package ex_04_Operators;
 
 public class Lab015_Interviewque {
     public static void main(String[] args) {
@@ -6,7 +6,7 @@ public class Lab015_Interviewque {
         String b = "World";
         int c = 10, d = 10;
         System.out.println(a + b + c + d);
-        System.out.println(c + d + a + b);
+        System.out.println(c + d + a + b + c + d);
 
     }
 }
