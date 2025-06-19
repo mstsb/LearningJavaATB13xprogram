23220a6407ab3a90e72b7816d3b5188c89416ec2
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-19 08:33:56 +0530

    Incrdec

---
+++ src/exam/incredec.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class incredec {
+    public static void main(String[] args) {
+            int a = 5;
+            System.out.println(a);
+            System.out.println(++a);
+            System.out.println(a++);
+            System.out.println(--a);
+            System.out.println(a--);
+
+    }
+}
