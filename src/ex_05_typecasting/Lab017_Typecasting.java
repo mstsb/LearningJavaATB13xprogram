d04ce7a06c26bbcec6c688046019685195126a5e
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-19 22:25:13 +0530

    Typecasting

---
+++ src/ex_05_typecasting/Lab017_Typecasting.java
@@ -0,0 +1,9 @@
+package ex_05_typecasting;
+
+public class Lab017_Typecasting {
+    public static void main(String[] args) {
+        int a = 300;
+        byte a1 = (byte)a;
+        System.out.println(a1);
+    }
+}
