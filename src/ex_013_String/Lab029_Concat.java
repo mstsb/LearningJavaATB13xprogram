70e7ae2311f3f46a2c8987297fa37a88cf196c50
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-24 22:29:55 +0530

    Concatenation

---
+++ src/ex_013_String/Lab029_Concat.java
@@ -0,0 +1,11 @@
+package ex_013_String;
+
+public class Lab029_Concat
+{
+    public static void main(String[] args)
+    {
+        String s = "Tanuja ";
+        s = s.concat("Singh");
+        System.out.println(s);
+    }
+}
