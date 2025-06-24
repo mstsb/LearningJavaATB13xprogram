dc8d5ead4da3a89b18f602a5112ef1aa1af701b5
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-24 22:31:06 +0530

    No para no return

---
+++ src/ex_012_functions/Lab027_no_para_no_return_type.java
@@ -0,0 +1,13 @@
+package ex_012_functions;
+
+public class Lab027_no_para_no_return_type
+{
+    public static void main(String[] args)
+    {
+        printmsg();
+    }
+    static void printmsg()
+    {
+        System.out.println("Hello");
+    }
+}
