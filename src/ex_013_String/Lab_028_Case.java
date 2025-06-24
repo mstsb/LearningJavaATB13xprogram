d5d7bc205068d3cadb501521b274612a4e3d38c3
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-24 22:25:08 +0530

    Case

---
+++ src/ex_013_String/Lab_028_Case.java
@@ -0,0 +1,13 @@
+package ex_013_String;
+
+public class Lab_028_Case
+{
+    public static void main(String[] args)
+    {
+            String s = "Tanuja";
+            System.out.println(s.toLowerCase());
+            System.out.println(s.toUpperCase());
+    }
+}
+
+
