fc101e75613ea0c39542a7f6380e0bb3b846bc73
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-25 22:42:31 +0530

    String method

---
+++ src/ex_013_String/Lab031_methods.java
@@ -0,0 +1,20 @@
+package ex_013_String;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Lab031_methods
+{
+    public static void main(String[] args) {
+        String s = "  Tanuja";
+        System.out.println(s);
+        System.out.println(s.contains("anu"));
+        System.out.println(s);
+        System.out.println(s.isEmpty());
+        System.out.println(s);
+        System.out.println(s.substring(1,4));
+        System.out.println(s);
+        System.out.println(s.endsWith("a"));
+        System.out.println(s.trim());
+        System.out.println(s);
+    }
+}
