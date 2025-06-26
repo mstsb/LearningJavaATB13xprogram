4f74da51aee1adf00bfe95e2eda3ffdd8000d8a5
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-06-26 17:02:04 +0530

    Array Program

---
+++ src/ex_014_array/Lab035_arrayProgram.java
@@ -0,0 +1,11 @@
+package ex_014_array;
+
+import java.util.Arrays;
+
+public class Lab035_arrayProgram {
+    public static void main(String[] args) {
+        int[] array = {2,1,3};
+        System.out.println(array.length);
+        Arrays.sort(array);
+    }
+}
