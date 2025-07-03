25ad56a2680e75b08f32c5e92e5862c7ff88f71c
TanujaSingh30 <singh.tanuja03@gmail.com>
2025-07-03 11:45:24 +0530

    foreach loop

---
+++ src/ex_08_for/Lab041_foreach.java
@@ -0,0 +1,14 @@
+package ex_08_for;
+
+import java.util.Arrays;
+
+public class Lab041_foreach {
+    public static void main(String[] args)
+    {
+        int[] array = {2,3,4,5};
+        for(Object o:array)
+        {
+            System.out.println(o);
+        }
+    }
+}
